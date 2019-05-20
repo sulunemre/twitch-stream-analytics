0482a7c9faa810742defe798a10b0ac36bfe0528
Emre Sülün <emresulun93@gmail.com>
2019-05-20 17:41:43 +0300

    Transfer secret strings to environment variables

---
+++ src/main/java/Util/Secrets.java
@@ -0,0 +1,6 @@
+package Util;
+
+public class Secrets {
+	public static final String MONGO_STRING = System.getenv("MONGO_STRING");
+	public static final String TWITCH_TOKEN = System.getenv("TWITCH_TOKEN");
+}
