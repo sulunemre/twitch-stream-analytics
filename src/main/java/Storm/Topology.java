f534a502c527885ee8b602fec57e36fcea9e034f
Emre Sülün <emresulun93@gmail.com>
2019-05-21 18:28:58 +0300

    Remove WebServer from Java

--- src/main/java/Storm/Topology.java
+++ src/main/java/Storm/Topology.java
@@ -1,21 +1,11 @@
 package Storm;
 
-import Util.WebServer;
 import org.apache.storm.Config;
 import org.apache.storm.LocalCluster;
 import org.apache.storm.topology.TopologyBuilder;
 
-import java.io.IOException;
-
 public class Topology {
 	public static void main(String... args) {
-		// Run web server
-		try {
-			new WebServer(8080);
-		} catch (IOException e) {
-			System.out.println("Could not run web server");
-		}
-
 		// Run Apache Storm
 		TopologyBuilder builder = new TopologyBuilder();
 		builder.setSpout("TwitchSpout", new TwitchSpout());
