329248c87ef45d37752461d0470fa9d00505080b
Yasin Balcancı <ybalcanci@gmail.com>
2019-05-22 00:10:46 +0300

    Node server sends data to clients using socket.io.

--- src/node/server.js
+++ src/node/server.js
@@ -1,85 +1,104 @@
-//{ emoticon_set: 0,
-//   channel_name: null,
-//   code: 'PogChamp',
-//   emotion: 'excited',
-//   channelOfMessage: 'cohhcarnage',
-//   _id: 88,
-//   channel_id: null,
-//   timestamp: '1558448502386' }
-var port = 5672;
 var amqp = require('amqplib/callback_api');
-var groupBy = require('json-groupby')
+var groupBy = require('json-groupby');
+var app = require('http').createServer(handler),
+	io = require('socket.io').listen(app),
+	fs = require('fs');
 
-var IPtoConnect = 'amqp://139.179.248.141';
+var refreshTimeInSeconds = 5;
+var slidingWindowIntervalInMinutes = 10;
+var port = 8081;
+var messageQueueIP = 'amqp://139.179.103.18';
+
 var channelsJSON;
 var messages = [];
 
-//remove messages that was sent more than 10 minutes ago.
-function filterMessagesBy10Minutes() {
-    messages = messages.filter(function (message) {
-        return (Date.now() - parseInt(message.timestamp)) / (60 * 1000) < 10;
-    });
-    groupMessagesByEmotions();
-
+/**
+ * Remove messages that was sent more than x minutes ago.
+ * @param minutes
+ */
+function filterMessagesByMinutes(minutes) {
+	messages = messages.filter(function (message) {
+		return (Date.now() - parseInt(message.timestamp)) / (60 * 1000) < minutes;
+	});
 }
 
-//Group and count the number of messages belonging to each emotion
-//and assign this to emotionsJSON variable.
+/**
+ * Group and count the number of messages belonging to each emotion
+ * and assign this to emotionsJSON variable.
+ */
 function groupMessagesByEmotions() {
-    channelsJSON = groupBy(messages, ['channelOfMessage'], ['emotion'])
-    console.log("************")
-    for (var channel in channelsJSON) {
-        for (let i = 0; i < channelsJSON[channel]["emotion"].length; i++) {
+	channelsJSON = groupBy(messages, ['channelOfMessage'], ['emotion'])
+	console.log("************")
+	for (var channel in channelsJSON) {
+		for (let i = 0; i < channelsJSON[channel]["emotion"].length; i++) {
 
-            //currentEmotion will be one of "disgust", "amused", "love" ...
-            let currentEmotion = channelsJSON[channel]["emotion"][i];
+			//currentEmotion will be one of "disgust", "amused", "love" ...
+			let currentEmotion = channelsJSON[channel]["emotion"][i];
 
-            //We don't need to keep emotions named "TO-BE-COMPLETED"
-            if (currentEmotion === "TO-BE-COMPLETED")
-                continue;
+			//We don't need to keep emotions named "TO-BE-COMPLETED"
+			if (currentEmotion === "TO-BE-COMPLETED")
+				continue;
 
-            //if we see this emotion first time in this chat
-            //give it number 1 else increment it.
-            if (channelsJSON[channel][currentEmotion] == undefined) {
-                channelsJSON[channel][currentEmotion] = 1;
-            } else {
-                channelsJSON[channel][currentEmotion] += 1;
-            }
-        }
-        //after traversing the list of emotions and counting,
-        //we don't need them as an array inside our JSON.
-        delete channelsJSON[channel]["emotion"];
-        //console.log(channelsJSON[channel])
-    }
-    console.log(channelsJSON)
+			//if we see this emotion first time in this chat
+			//give it number 1 else increment it.
+			if (channelsJSON[channel][currentEmotion] == undefined) {
+				channelsJSON[channel][currentEmotion] = 1;
+			} else {
+				channelsJSON[channel][currentEmotion] += 1;
+			}
+		}
+		//after traversing the list of emotions and counting,
+		//we don't need them as an array inside our JSON.
+		delete channelsJSON[channel]["emotion"];
+	}
+	console.log(channelsJSON)
 }
 
-amqp.connect(IPtoConnect, function (error0, connection) {
+amqp.connect(messageQueueIP, function (error0, connection) {
+	setInterval(function () {
+		filterMessagesByMinutes(slidingWindowIntervalInMinutes);
+		groupMessagesByEmotions();
+	}, refreshTimeInSeconds * 1000) // filter messages every x seconds
 
-    setInterval(filterMessagesBy10Minutes, 5 * 1000) // filter messages every 5 minutes
+	connection.createChannel(function (error1, channel) {
+		var queueName = 'hello';
 
-    if (error0) {
-        throw error0;
-    }
-    connection.createChannel(function (error1, channel) {
-        if (error1) {
-            throw error1;
-        }
+		channel.assertQueue(queueName, {
+			durable: false
+		});
 
-        var queue = 'hello';
+		console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queueName);
 
-        channel.assertQueue(queue, {
-            durable: false
-        });
+		channel.consume(queueName, function (msg) {
+			var message = JSON.parse(msg.content.toString());
+			messages.push(message);
+		}, {
+			noAck: true
+		});
+	});
+});
 
-        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+// Start listening to the clients on port.
+app.listen(port);
 
-        channel.consume(queue, function (msg) {
-            var message = JSON.parse(msg.content.toString());
-            messages.push(message);
-            //console.log(JSON.stringify(message));
-        }, {
-            noAck: true
-        });
-    });
+function handler(req, res) {
+	fs.readFile(__dirname + '/index.html',
+		function (err, data) {
+			if (err) {
+				res.writeHead(500);
+				return res.end('Error loading index.html');
+			}
+			res.writeHead(200);
+			res.end(data);
+		});
+}
+
+// Manage connections
+io.sockets.on('connection', function (socket) {
+	var periodInMilliseconds = refreshTimeInSeconds * 1000;
+	setInterval(sendJsonToClient, periodInMilliseconds);
+
+	function sendJsonToClient() {
+		socket.emit('server request', channelsJSON);
+	}
 });
