1ebd07c7334032b649526b5fe1d01ad8aa8057d7
Adahan Yalçınkaya <adahan.yalcinkaya@gmail.com>
2019-05-23 23:57:14 +0300

    Çarşamba emotes collection from MongoDB to speed up look up

--- src/main/java/Storm/EmoteBolt.java
+++ src/main/java/Storm/EmoteBolt.java
@@ -2,6 +2,7 @@ package Storm;
 
 import Database.MongoConnection;
 import com.mongodb.MongoWriteException;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import org.apache.storm.topology.BasicOutputCollector;
 import org.apache.storm.topology.OutputFieldsDeclarer;
@@ -18,81 +19,96 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.Scanner;
+import java.util.*;
 
 import static com.mongodb.client.model.Filters.eq;
 
 public class EmoteBolt extends BaseBasicBolt {
-	@Override
-	public void execute(Tuple tuple, BasicOutputCollector basicOutputCollector) {
-		MongoCollection<Document> emotesCollection = MongoConnection.getDatabase().getCollection("emotes");
+    private Map<String, String> emoteIDToJSON;
 
-		Optional<String> rawEmotes = (Optional<String>) tuple.getValueByField("rawEmotes");
-		String channelOfMessage = (String) tuple.getValueByField("channelOfMessage");
-		if (rawEmotes.isPresent()) {
-			String emotesString = rawEmotes.get();
-			String[] emotes = emotesString.split("/");
-			List<String> emoteIds = new ArrayList<>();
-			for (String e : emotes)
-				emoteIds.add(e.split(":")[0]);
+    EmoteBolt() {
+        emoteIDToJSON = new HashMap();
+        fillInHashMap();
+    }
 
-			for (String emoteId : emoteIds) {
-				try {
-					String emoteJson = getEmoteDetailsFromDB(emoteId);
-					if (emoteJson == null) {
-						System.out.println("Emote is not in the database");
-						continue;
-					}
-					basicOutputCollector.emit(new Values(emoteJson, channelOfMessage));
+    private void fillInHashMap() {
+        System.out.println("Started caching emotes.");
+        MongoCollection<Document> emotesCollection = MongoConnection.getDatabase().getCollection("emotes");
+        FindIterable<Document> docs = emotesCollection.find();
+        for (Document doc : docs)
+            emoteIDToJSON.put(doc.get("_id").toString(), doc.toJson());
+
+        System.out.println("Done caching emotes.");
+    }
+
+    @Override
+    public void execute(Tuple tuple, BasicOutputCollector basicOutputCollector) {
+        MongoCollection<Document> emotesCollection = MongoConnection.getDatabase().getCollection("emotes");
+
+        Optional<String> rawEmotes = (Optional<String>) tuple.getValueByField("rawEmotes");
+        String channelOfMessage = (String) tuple.getValueByField("channelOfMessage");
+        if (rawEmotes.isPresent()) {
+            String emotesString = rawEmotes.get();
+            String[] emotes = emotesString.split("/");
+            List<String> emoteIds = new ArrayList<>();
+            for (String e : emotes)
+                emoteIds.add(e.split(":")[0]);
+
+            for (String emoteId : emoteIds) {
+                try {
+                    String emoteJson = emoteIDToJSON.get(emoteId);
+                    //String emoteJson = getEmoteDetailsFromDB(emoteId);
+                    if (emoteJson == null) {
+                        System.out.println("Emote is not in the database");
+                        continue;
+                    }
+                    basicOutputCollector.emit(new Values(emoteJson, channelOfMessage));
 
 //					BasicDBObject toBeInserted = BasicDBObject.parse(emoteJson);
 //					emotesCollection.insertOne(new Document(toBeInserted.toMap())); // DONT ADD NEW EMOTES, IT'S ENOUGH
 //					System.out.println("A new emote inserted");
-				} catch (MongoWriteException e) {
-					System.out.println("An emote already exists");
-				} catch (JSONException | NullPointerException ignored) {
-				}
-			}
-		}
+                } catch (MongoWriteException e) {
+                    System.out.println("An emote already exists");
+                } catch (JSONException | NullPointerException ignored) {
+                }
+            }
+        }
 
-	}
+    }
 
-	@Override
-	public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
-		outputFieldsDeclarer.declare(new Fields("emoteJson", "channelOfMessage"));
-	}
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+        outputFieldsDeclarer.declare(new Fields("emoteJson", "channelOfMessage"));
+    }
 
-	/**
-	 * Query api.twitchemotes.com API and download the details of emote
-	 *
-	 * @param emoteId global unique emote id
-	 * @return JSON object string that contains emote details
-	 */
-	private static String getEmoteDetailsFromWeb(String emoteId) {
-		try {
-			JSONArray response = new JSONArray(new Scanner(new URL("https://api.twitchemotes.com/api/v4/emotes?id=" + emoteId).openStream(), StandardCharsets.UTF_8).useDelimiter("\\A").next());
-			JSONObject emoteJson = response.getJSONObject(0);
-			// Replace id with _id
-			emoteJson.put("_id", new ObjectId((String) emoteJson.get("_id")));
-			emoteJson.remove("id");
-			emoteJson.put("emotion", "TO-BE-COMPLETED");
+    /**
+     * Query api.twitchemotes.com API and download the details of emote
+     *
+     * @param emoteId global unique emote id
+     * @return JSON object string that contains emote details
+     */
+    private static String getEmoteDetailsFromWeb(String emoteId) {
+        try {
+            JSONArray response = new JSONArray(new Scanner(new URL("https://api.twitchemotes.com/api/v4/emotes?id=" + emoteId).openStream(), StandardCharsets.UTF_8).useDelimiter("\\A").next());
+            JSONObject emoteJson = response.getJSONObject(0);
+            // Replace id with _id
+            emoteJson.put("_id", new ObjectId((String) emoteJson.get("_id")));
+            emoteJson.remove("id");
+            emoteJson.put("emotion", "TO-BE-COMPLETED");
 
-			return emoteJson.toString();
-		} catch (IOException | JSONException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
+            return emoteJson.toString();
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
-	private static String getEmoteDetailsFromDB(String emoteId) {
-		MongoCollection<Document> emotesCollection = MongoConnection.getDatabase().getCollection("emotes");
-		Document document = emotesCollection.find(eq("_id", Long.parseLong(emoteId))).first();
-		if (document == null)
-			return null;
+    private static String getEmoteDetailsFromDB(String emoteId) {
+        MongoCollection<Document> emotesCollection = MongoConnection.getDatabase().getCollection("emotes");
+        Document document = emotesCollection.find(eq("_id", Long.parseLong(emoteId))).first();
+        if (document == null)
+            return null;
 
-		return document.toJson();
-	}
+        return document.toJson();
+    }
 }
